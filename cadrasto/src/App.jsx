154355cb7398975ffe07e026e976282ce3a7fcbe
Isaac Alves <Isaacsantos.7r@gmail.com>
2026-09-10 18:34:15 -0300

    v1 react

---
+++ cadrasto/src/App.jsx
@@ -0,0 +1,21 @@
+export function App() {
+const helou = "React"
+
+  return (
+    <>
+      <h1>{helou}</h1>
+
+    </>
+  )
+}
+export function App1() {
+  const helou = 1
+  const helou1 = 1
+  return (
+    <>
+      <h2>Deu ${helou + helou1} </h2>
+    </>
+  )
+}
+
+

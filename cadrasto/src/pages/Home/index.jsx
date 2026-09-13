68b769dc305102ce6f7e0fde6de8c37bb28edd2b
Isaac Alves <Isaacsantos.7r@gmail.com>
2026-09-13 14:29:50 -0300

    adiciona funcionalidade de cadastro de usuário com campos de nome, idade e email

--- cadrasto/src/pages/Home/index.jsx
+++ cadrasto/src/pages/Home/index.jsx
@@ -1,9 +1,25 @@
+import { useRef } from "react"
+
 import { Conteiner, TopBackground, Form, Title, ConteinerInput, ConteinerInputEmail, Input, Inputlabel, Button } from "./styles"
 
 import imgHome from '../../assets/users.png'
 
 export function Home() {
+
+  const inputName = useRef()
+  const inputAge = useRef()
+  const inputEmail = useRef()
+
+  function createNewUser() {
+
+    console.log(inputName.current.value)
+     console.log(inputAge.current.value)
+      console.log(inputEmail.current.value)
+
+  }
+
   return (
+
     <Conteiner>
 
       <TopBackground>
@@ -17,21 +33,21 @@ export function Home() {
         <div>
 
           <Title>Cadrastrar Usuario</Title>
-          
+
         </div>
 
         <ConteinerInput>
 
           <div>
             <Inputlabel>Name<span>*</span></Inputlabel>
-            <Input type="text" placeholder="Name do usuario" />
+            <Input type="text" placeholder="Name do usuario" ref={inputName} />
 
           </div>
 
 
           <div>
             <Inputlabel>Age<span>*</span></Inputlabel>
-            <Input type="Number" placeholder="Age do usuario" />
+            <Input type="Number" placeholder="Age do usuario" ref={inputAge} />
 
           </div>
 
@@ -41,13 +57,13 @@ export function Home() {
 
           <div>
             <Inputlabel>email<span>*</span></Inputlabel>
-            <Input type="email" placeholder="Email do usuario" />
+            <Input type="email" placeholder="Email do usuario" ref={inputEmail} />
           </div>
 
         </ConteinerInputEmail>
 
 
-        <Button>Cadrasto Usuario</Button>
+        <Button type="button" onClick={createNewUser} >Cadrasto Usuario</Button>
 
       </Form>
 

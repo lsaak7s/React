1f37361429ec5c6e999cce4e7d4aae8e198c20eb
Isaac Alves <Isaacsantos.7r@gmail.com>
2026-09-18 21:24:50 -0300

    substitui imagem antiga por nova e ajusta estrutura do componente ListUsers para exibir usuários

---
+++ cadrasto/src/pages/Home/listUsers/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+background-color:#7f3841;
+display:;
+
+
+
+
+`
+
+export const Title = styled.h2``
+
+export const ConteinerUser = styled.div``
+
+export const CardUser = styled.div``
+
+export const AvatarUser = styled.div``
+
+export const TrashIcon = styled.div``
